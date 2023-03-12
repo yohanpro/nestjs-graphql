a5b4b7c388470cc6755a9212de6d1f67ed84e7d0
Yohan Kim <yohan@yohanpro.com>
2023-03-12 14:08:24 +0900

    yarn3 마이그레이션

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,7 @@ import { CatModule } from './cat/cat.module';
 import { CatResolver } from './cat/cat.resolver';
 
 @Module({
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: true, driver: 'graphql' })],
-  // imports: [],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
